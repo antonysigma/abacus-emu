eb2622a545a1fbbf9b0e153bd40ac2dbe038e56f
Antony Chan <antony.sigma@gmail.com>
2024-08-11 15:09:37 -0700

    use international math symbol

--- src/components/keypad.vue
+++ src/components/keypad.vue
@@ -2,15 +2,26 @@
     <form @submit.prevent="onSubmit">
         <input maxlength="10" id="a" v-model="a"/>
         <select v-model="operator">
-            <option>plus</option>
-            <option>minus</option>
-            <option>times</option>
-            <option>divide by</option>
+            <option value="plus">&plus;</option>
+            <option value="minus">&minus;</option>
+            <option value="times">&times;</option>
+            <option value="divide by">&#x00F7;</option>
         </select>
         <input maxlength="10" id="b" v-model="b" />
         <input value="Execute" type="submit"/>
     </form>
 </template>
+
+<style>
+form input {
+    font-size: 120%;
+    width: 7em;
+}
+
+select {
+    font-size: 170%;
+}
+</style>
 
 <script setup>
 import {a, b, operator} from '../models';
