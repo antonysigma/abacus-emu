2b79ed1c8347d2a19462316467c545114a579f83
Antony Chan <antony.sigma@gmail.com>
2024-08-14 21:38:12 -0700

    Limit the instruction view

--- src/components/instruction_list.vue
+++ src/components/instruction_list.vue
@@ -1,5 +1,5 @@
 <template>
-    <ol>
+    <ol class="instruction_view">
         <li v-for="[index, inst] of instruction_steps.entries()" :key="index">{{ inst }}</li>
     </ol>
 </template>
@@ -7,3 +7,10 @@
 <script setup>
 import {instruction_steps} from '../models';
 </script>
+
+<style>
+.instruction_view {
+    height: 50vh;
+    overflow-y: scroll;
+}
+</style>
